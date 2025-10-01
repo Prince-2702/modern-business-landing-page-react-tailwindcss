4f17041a781f79306c0eb1782757a52a42ee4bde
Prince-2702 <princeundaviya@gmail.com>
2025-10-01 18:34:13 +0530

    Update File

--- src/Components/PurposeSection.jsx
+++ src/Components/PurposeSection.jsx
@@ -1,51 +1,71 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../ultils/motion";
 
 const PurposeSection = () => {
-
   const features = [
     {
-      icon: "🟣", 
+      icon: "🟣",
       title: "Built for impact",
-      description: "We identify and nurture a truly diverse team of designers, developers and marketers"
+      description:
+        "We identify and nurture a truly diverse team of designers, developers and marketers",
     },
     {
-      icon: "🔴", 
+      icon: "🔴",
       title: "In sync with you",
-      description: "We work the way you do by adapting to your workflows and rhythm we aim to blend in for a seamless."
-    }
+      description:
+        "We work the way you do by adapting to your workflows and rhythm we aim to blend in for a seamless.",
+    },
   ];
 
   return (
-    <section id='about' className='w-full bg-gray-50 py-16 px-4 md:px-8 sm:px-6'>
-        <div className='max-w-6xl mx-auto'>
-           <div className='grid md:grid-cols-3 grid-cols-1 gap-8'>
-             <div className=''>
-                <p className='text-sm text-purple-600 font-medium mb-2'>ACTIVE MORE</p>
-                <h2 className='text-3xl md:w-4/5 w-full md:text-4xl font-bold text-gray-900'>Purpose of a convay is to keep your team</h2>
-            </div>
+    <section
+      id="about"
+      className="w-full bg-gray-50 py-16 px-4 md:px-8 sm:px-6"
+    >
+      <div className="max-w-6xl mx-auto">
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-8">
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className=""
+          >
+            <p className="text-sm text-purple-600 font-medium mb-2">
+              ACTIVE MORE
+            </p>
+            <h2 className="text-3xl md:w-4/5 w-full md:text-4xl font-bold text-gray-900">
+              Purpose of a convay is to keep your team
+            </h2>
+          </motion.div>
 
-            {/* bullet points*/}
-            <div className='col-span-2 grid grid-cols-1 md:grid-cols-2 justify-between gap-8'>
-              {
-                features.map((feature, index) => (
-                  <div key={index} className='flex space-x-4'>
-                    <div className='w-12 h-12 flex items-center justify-start rounded-lg'>{feature.icon}</div>
+          {/* bullet points*/}
+          <motion.div
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView="show"
+            className="col-span-2 grid grid-cols-1 md:grid-cols-2 justify-between gap-8"
+          >
+            {features.map((feature, index) => (
+              <div key={index} className="flex space-x-4">
+                <div className="w-12 h-12 flex items-center justify-start rounded-lg">
+                  {feature.icon}
+                </div>
 
-                    <div>
-                      <h3 className='text-xl font-semibold text-gray-900 mb-2'>{feature.title}</h3>
-                      <p className='text-gray-600'>{feature.description}</p>
-                    </div>
-                  </div>
-                ))
-
-              }
-            </div>
-
-
-           </div>
+                <div>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-2">
+                    {feature.title}
+                  </h3>
+                  <p className="text-gray-600">{feature.description}</p>
+                </div>
+              </div>
+            ))}
+          </motion.div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default PurposeSection
+export default PurposeSection;
