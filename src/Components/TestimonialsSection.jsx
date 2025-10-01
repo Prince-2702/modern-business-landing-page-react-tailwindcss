d75e4b2101d12b02691159d0b9254acdcf71ede8
Prince-2702 <princeundaviya@gmail.com>
2025-10-01 17:56:58 +0530

    Update Testimonial Section

--- src/Components/TestimonialsSection.jsx
+++ src/Components/TestimonialsSection.jsx
@@ -48,7 +48,7 @@ const testimonials = [
 
 const TestimonialsSection = () => {
   return (
-    <section className="py-16 px-4 max-w-7xl mx-auto">
+    <section className="py-16 px-4 max-w-7xl mx-auto" id="testimonials">
       <div className="text-center mb-12">
         <h2 className="text-3xl font-bold md:text-4xl mb-4">
           What our happy client say
